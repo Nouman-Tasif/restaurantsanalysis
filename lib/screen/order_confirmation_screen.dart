deffa7871422c8e6f23bb92ec1780e8d3ed67df3
Noman Tasif <nouman.tasif@evampsaanga.com>
2024-07-31 07:28:07 +0500

    code updated

--- lib/screen/order_confirmation_screen.dart
+++ lib/screen/order_confirmation_screen.dart
@@ -33,90 +33,92 @@ class OrderConfirmationScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Order Confirmation'),
       ),
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/foodorderpicture.jpg'),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsetsDirectional.only(top: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset(
-                  menuItem.imageUrl,
-                  width: MediaQuery.of(context).size.width,
-                  height: 200,
+      body:SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/foodorderpicture.jpg'),
                   fit: BoxFit.cover,
                 ),
-                SizedBox(height: 20),
-                Container(
-                  color: Colors.black54, // Semi-transparent background
-                  width: DynamicSize.width(0.9, context),
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Order Details',
-                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        'Item: ${order.menuItem.name}',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
-                      Text(
-                        'Price: ${order.menuItem.price} USD',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
-                      Text(
-                        'Sodium: ${order.menuItem.sodium} g',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
-                      Text(
-                        'Allergens: ${order.menuItem.allergens.join(', ')}',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
-                      Text(
-                        'Fat: ${order.menuItem.fat} g',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
-                      Text(
-                        'Calories: ${order.menuItem.calories} g',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
-
-                    ],
-                  ),
-                ),
-                SizedBox(height: 20),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      await _placeOrder();
-                      Navigator.pop(context);
-                    },
-                    child: Text('Place Order'),
-                  ),
-                ),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text('Cancel'),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsetsDirectional.only(top: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    menuItem.imageUrl,
+                    width: MediaQuery.of(context).size.width,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    color: Colors.black54, // Semi-transparent background
+                    width: DynamicSize.width(0.9, context),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Order Details',
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'Item: ${order.menuItem.name}',
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                        Text(
+                          'Price: ${order.menuItem.price} USD',
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                        Text(
+                          'Sodium: ${order.menuItem.sodium} g',
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                        Text(
+                          'Allergens: ${order.menuItem.allergens.join(', ')}',
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                        Text(
+                          'Fat: ${order.menuItem.fat} g',
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                        Text(
+                          'Calories: ${order.menuItem.calories} g',
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await _placeOrder();
+                        Navigator.pop(context);
+                      },
+                      child: Text('Place Order'),
+                    ),
+                  ),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Cancel'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
